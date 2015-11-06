936a6007708bad438d511eb8f810659ff24bdd12
theDarkForce <theDarkForce@aliyun.com>
2015-11-06 19:22:01 +0800

    Signed-off-by: theDarkForce <theDarkForce@aliyun.com>

--- gethtml.py
+++ gethtml.py
@@ -5,13 +5,15 @@
 
 import urllib2
 import HTMLParser
-from dom import *
 from doclex import doclex
+import time
 import pymongo
+import chardet
 
 url = "http://jj.hbtv.com.cn/"
 
 collection = None
+collection_url_profile = None
 
 '''
 def get_data(str):
@@ -77,11 +79,16 @@ def get_link(str):
     return url
 '''
 
+def judged_url(url):
+    if url.find('http') == -1:
+        return False
+    return True
+
 class htmlprocess(HTMLParser.HTMLParser):
     def __init__(self, url):
         self.link = []
         self.data = []
-        self.key_url = []
+        self.key_url = {}
 
         self.url = url
         self.link_url = ""
@@ -96,21 +103,31 @@ class htmlprocess(HTMLParser.HTMLParser):
         if tag == 'a':
             for name,value in attrs:
                 if name == 'href':
+                    if not judged_url(value):
+                        value = self.url + value
                     self.link_url = value
                     self.link.append(value)
 
     def handle_data(self, data):
         if self.current_tag == 'title':
             keys = doclex.simplesplit(data)
-            for key in keys:
-                self.key_url.append({'key':key, 'url': self.url})
+            if isinstance(keys, list) and len(keys) > 0:
+                for key in keys:
+                    if not self.key_url.has_key(key):
+                        self.key_url[key] = []
+                    self.key_url[key].append(self.url)
         elif self.current_tag == 'a':
+            if not judged_url(self.link_url):
+                self.link_url = self.url + self.link_url
             keys = doclex.simplesplit(data)
             if isinstance(keys, list) and len(keys) > 0:
                 for key in keys:
-                    self.key_url.append({'key':key, 'url': self.link_url})
+                    if not self.key_url.has_key(key):
+                        self.key_url[key] = []
+                    self.key_url[key].append(self.link_url)
         else:
-            self.data.append(data)
+            if self.current_tag == 'p' or self.current_tag == 'div':
+                self.data.append(data)
 
 def process_data(data):
     return doclex.lex(data)
@@ -123,34 +140,172 @@ def get_page(url):
 
         return the_page
     except:
-        print 'get_page error', url
+        #import traceback
+        #traceback.print_exc()
+        #print 'get_page error', url
+        pass
 
 def process_link(url):
-    process_page(url, get_page(url))
+    key_url, url_profile_dict = process_url_real(url, [])
+
+    for key, value in url_profile_dict.iteritems():
+        encoding = chardet.detect(value)
+        str = unicode(value, encoding['encoding'])
+        collection_url_profile.insert({'key':key, 'urlprofile':str, 'timetmp':time.time()})
+
+def process_url_real(url, process_link_list):
+    if url in process_link_list:
+        return
+
+    process_link_list.append(url)
+    url_profile_dict = {}
+
+    data = get_page(url)
+    if data is None:
+        return
+
+    pageinfo = process_page(url, data)
+    if pageinfo is None:
+        return
+
+    link_list, key_url1, url_profile = pageinfo
+    url_profile_dict[url] = url_profile
+
+    key_url = {}
+
+    def process_sub_link_list(_data_list, process_link_list):
+        link_list = []
+        key_url = {}
+        url_profile_dict = {}
+
+        for url, data in _data_list.iteritems():
+            if url in process_link_list:
+                continue
+
+            process_link_list.append(url)
+
+            pageinfo = process_page(url, data)
+            if pageinfo is not None:
+                linklist, keyurl, urlprofile = pageinfo
+                link_list.extend((linklist))
+                for key, value in keyurl.iteritems():
+                    if key_url.has_key(key):
+                        key_url[key] += value
+                    else:
+                        key_url[key] = value
+                url_profile_dict[url] = urlprofile
+
+        return link_list, key_url, url_profile_dict
+
+    def lprocess_link_list(data_list, key_url, process_link_list):
+        while isinstance(data_list, dict) and len(data_list) > 0:
+            linklist, keyurl, urlprofile = process_sub_link_list(data_list, process_link_list)
+
+            data_list = {}
+            for url in linklist:
+                data = get_page(url)
+                if data is not None:
+                    data_list[url] = data
+
+            for key, value in keyurl.iteritems():
+                if not key_url.has_key(key):
+                    key_url[key] = []
+
+                for url in value:
+                    if not data_list.has_key(url):
+                        data = get_page(url)
+                        if data is not None:
+                            data_list[url] = data
+                        else:
+                            continue
+
+                    if url not in key_url[key]:
+                        key_url[key].append(url)
+                        collection.insert({'key':key, 'url':url, 'timetmp':time.time()})
+
+            url_profile_dict.update(urlprofile)
+
+        return url_profile_dict
+
+    data_list = {}
+    for key, value in key_url1.iteritems():
+        if not key_url.has_key(key):
+            key_url[key] = []
+
+        for url in value:
+            if not data_list.has_key(url):
+                data = get_page(url)
+                if data is not None:
+                    data_list[url] = data
+                else:
+                    continue
+
+            if url not in key_url[key]:
+                key_url[key].append(url)
+                collection.insert({'key':key, 'url':url, 'timetmp':time.time()})
+
+            if len(data_list) > 100:
+                url_profile_dict.update(lprocess_link_list(data_list, key_url, process_link_list))
+                data_list = {}
+
+    for url in link_list:
+        if not data_list.has_key(url):
+            data = get_page(url)
+            if data is not None:
+                data_list[url] = data
+
+            if len(data_list) > 100:
+                url_profile_dict.update(process_link_list(data_list, key_url, process_link_list))
+                data_list = {}
+
+    return url_profile_dict
 
 def process_page(url, data):
     if data is None:
         return
 
-    htmlp = htmlprocess(url)
-    htmlp.feed(data)
+    try:
+        key_url = {}
+        url_profile = ""
 
-    for it in htmlp.key_url:
-        collection.insert(it)
+        htmlp = htmlprocess(url)
+        htmlp.feed(data)
 
-    for data in htmlp.data:
-        if len(data) > 32:
-            keys = doclex.simplesplit(data)
-            if isinstance(keys, list) and len(keys) > 0:
-                for key in keys:
-                    collection.insert({'key':key, 'url': url})
-        else:
-            keys1 = process_data(data)
-            for key1 in keys1:
-                collection.insert({"key":key1,"url":url})
+        key_url.update(htmlp.key_url)
+        if len(key_url) > 0:
+            for key, value in key_url.iteritems():
+                if len(value) > 0:
+                    urllist = []
+                    urllist = [url for url in value if urllist.count(url) == 0]
+                    if url not in key_url[key]:
+                        key_url[key] = urllist
 
-    for url1 in htmlp.link:
-        process_link(url1)
+        for data in htmlp.data:
+            if len(data) < 32:
+                keys = doclex.simplesplit(data)
+                if isinstance(keys, list) and len(keys) > 0:
+                    for key in keys:
+                        if not key_url.has_key(key):
+                            key_url[key] = []
+                        if url not in key_url[key]:
+                            key_url[key].append(url)
+            else:
+                if len(data) > 100:
+                    url_profile = data[0:len(data) if len(data) < 100 else 100] + "..."
+                keys1 = process_data(data)
+                for key1 in keys1:
+                    if not key_url.has_key(key1):
+                        key_url[key1] = []
+                    if url not in key_url[key1]:
+                        key_url[key1].append(url)
+
+        return htmlp.link, key_url, url_profile
+
+    except:
+        import traceback
+        traceback.print_exc()
+        #print 'get_page error', url
+        pass
 
 
 #process_link(url)
